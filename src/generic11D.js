1d8cae3e004b0cd6490d29362418308fbff567bf
Alexis Munsayac <alexis.munsayac@gmail.com>
2019-03-07 07:37:03 +0800

    Create generic11D.js

---
+++ src/generic11D.js
@@ -0,0 +1,49 @@
+/**
+ * @license
+ * MIT License
+ *
+ * Copyright (c) 2019 Alexis Munsayac
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ *
+ *
+ * @author Alexis Munsayac <alexis.munsayac@gmail.com>
+ * @copyright Alexis Munsayac 2019
+ */
+const { sin, floor } = Math;
+
+const rand = x => (sin(x) * 43758.5453123) % 1;
+
+const mix = (x, y, t) => x + (y - x) * t;
+
+/**
+ * @memberof Unpleasant
+ * @description
+ * 1-dimensional Generic 1 Noise
+ * @see {@link https://gist.github.com/patriciogonzalezvivo/670c22f3966e662d2f83|GLSL Noise}
+ * @param {Number} x
+ * @returns {Number}
+ */
+const generic11D = (x) => {
+  const fl = floor(x);
+  const fc = x % 1;
+
+  return mix(rand(fl), rand(fl + 1.0), fc);
+};
+
+export default generic11D;
